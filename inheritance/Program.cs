c5c5a86f86e853a17018be7ba7dbbce25aa21ba7
ilyas <ilyas@LAPTOP-NOQ3L7Q3>
2017-02-20 22:11:42 -0600

    Add project files.

---
+++ inheritance/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Principal p = new Principal();
+            p.Monitor();
+            Teacher t = new Teacher();
+            t.Monitor();
+            t.Teach();
+            Student s = new Student();
+            s.Monitor();
+            s.Learn();
+            Console.ReadKey();
+        }
+        class Principal
+        {
+            public void Monitor()
+            {
+                Console.WriteLine("Monitor");
+            }
+        }
+        class Teacher : Principal
+        {
+            public void Teach()
+            {
+                Console.WriteLine("Teach");
+            }
+        }
+        class Student : Principal
+        {
+            public void Learn()
+            {
+                Console.WriteLine("Learn");
+            }
+        }
+    }
+}
